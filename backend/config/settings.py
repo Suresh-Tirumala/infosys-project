3ae27fb60a40de1083fb82650b94bea6c9a1578a
Suresh-Tirumala <sureshtirumala466@gmail.com>
2026-08-17 20:48:19 +0530

    Remove PostgreSQL, use SQLite only

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -62,16 +62,6 @@ DATABASES = {
     }
 }
 
-DATABASE_URL = os.environ.get('DATABASE_URL')
-if DATABASE_URL:
-    import dj_database_url
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=DATABASE_URL,
-            conn_max_age=600,
-        )
-    }
-
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
